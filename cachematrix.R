496de320f51dd125f3ed658bc12fb43b8e649f5c
anewface <atenzaandam@gmail.com>
2015-07-25 20:08:16 -0400

    Assignment 2
    Aten

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,89 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set<- function (y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function () x
+	setinverse <- function(inverese)
+	inv <<- inverse
+	getinverse <- function () inv
+	list(set=set, get=get, setinverse=serinverse, getinverse = getinverse)
 }
+## This function returns the inverse of the matrix. 
+## It first checks if if the inverse has allready been computed and if so returns the allready computed value.
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+CacheSolve <- function (x, ...) {
+	inv <- x$getinverse(
+	if(!is.null(inv)) {
+		return(inv)
+	}
+	data <- x$get(
+	inv <- solve(data)
+	x$setinverse(inv)
+	inv
 }
+## This function creates a function of the provided data where if the data in not a missing value an inverse for each data point is recieved
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
